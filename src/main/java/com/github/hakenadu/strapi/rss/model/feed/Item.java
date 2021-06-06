b4ed8d7a42f4c6dc88e27c533b34f6ffd0e4b2d7
Manuel Seiche <hakenadu91@gmail.com>
2021-06-06 19:34:46 +0200

    pub date support + optional item values

--- src/main/java/com/github/hakenadu/strapi/rss/model/feed/Item.java
+++ src/main/java/com/github/hakenadu/strapi/rss/model/feed/Item.java
@@ -1,6 +1,7 @@
 package com.github.hakenadu.strapi.rss.model.feed;
 
 import java.net.URI;
+import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
@@ -8,18 +9,60 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public final class Item {
 
-	@JacksonXmlProperty
-	private String title;
+	/*
+	 * REQUIRED
+	 */
 
 	@JacksonXmlProperty
-	private String description;
+	private final String title;
 
 	@JacksonXmlProperty
-	private URI link;
+	private final String description;
 
-	public Item(final String title, final String description, final URI link) {
+	@JacksonXmlProperty
+	private final URI link;
+
+	/*
+	 * Optional
+	 */
+
+	@JacksonXmlProperty
+	private final String author;
+
+	@JacksonXmlProperty
+	private final String category;
+
+	@JacksonXmlProperty
+	private final String enclosure;
+
+	@JacksonXmlProperty
+	private final String guid;
+
+	/**
+	 * String for simplicity in serialization... validation will happen in
+	 * ItemFactory
+	 */
+	@JacksonXmlProperty
+	private final String pubDate;
+
+	@JacksonXmlProperty
+	private final String source;
+
+	public Item(final String title, final String description, final URI link, final String author,
+			final String category, final String enclosure, final String guid, final String pubDate,
+			final String source) {
+		Objects.requireNonNull(title, "no title for item passed");
+		Objects.requireNonNull(description, "no description for item passed");
+		Objects.requireNonNull(link, "no link for item passed");
+
 		this.title = title;
 		this.description = description;
 		this.link = link;
+		this.author = author;
+		this.category = category;
+		this.enclosure = enclosure;
+		this.guid = guid;
+		this.pubDate = pubDate;
+		this.source = source;
 	}
 }
--- src/main/java/com/github/hakenadu/strapi/rss/service/feed/ItemFactory.java
+++ src/main/java/com/github/hakenadu/strapi/rss/service/feed/ItemFactory.java
@@ -1,6 +1,9 @@
 package com.github.hakenadu.strapi.rss.service.feed;
 
 import java.net.URI;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -17,14 +20,43 @@ public final class ItemFactory {
 
 	private static final Pattern JSON_COORDINATE_MATCHER = Pattern.compile("<<([^\\[\\]\\s]+)>>");
 
+	private static final DateTimeFormatter TARGET_DATE_FORMATTER = DateTimeFormatter.RFC_1123_DATE_TIME;
+
+	/*
+	 * Required
+	 */
+
 	private final String title;
 	private final String description;
 	private final String link;
 
-	public ItemFactory(final String title, final String description, final String link) {
+	/*
+	 * Optional
+	 */
+
+	private final String author;
+	private final String category;
+	private final String enclosure;
+	private final String guid;
+	private final String pubDate;
+	private final String source;
+
+	public ItemFactory(final String title, final String description, final String link, final String author,
+			final String category, final String enclosure, final String guid, final String pubDate,
+			final String source) {
+		Objects.requireNonNull(title, "no title pattern for item-factory passed");
+		Objects.requireNonNull(description, "no description pattern for item-factory passed");
+		Objects.requireNonNull(link, "no link pattern for item-factory passed");
+
 		this.title = title;
 		this.description = description;
 		this.link = link;
+		this.author = author;
+		this.category = category;
+		this.enclosure = enclosure;
+		this.guid = guid;
+		this.pubDate = pubDate;
+		this.source = source;
 	}
 
 	private String resolveJsonCoordinate(final JsonNode entry, final String jsonCoordinate) {
@@ -51,7 +83,18 @@ public final class ItemFactory {
 	}
 
 	public Item create(final JsonNode entry) {
-		return new Item(resolveItemValue(entry, title), resolveItemValue(entry, description),
-				URI.create(resolveItemValue(entry, link)));
+		return new Item(resolveItemValue(entry, title), // required
+				resolveItemValue(entry, description), // required
+				URI.create(resolveItemValue(entry, link)), // required
+				resolveItemValue(entry, author), // optional
+				resolveItemValue(entry, category), // optional
+				resolveItemValue(entry, enclosure), // optional
+				resolveItemValue(entry, guid), // optional
+				Optional.ofNullable(resolveItemValue(entry, pubDate))
+						.map(pubDateString -> ZonedDateTime.parse(pubDateString))
+						.map(intermediateLocalDateTime -> intermediateLocalDateTime.format(TARGET_DATE_FORMATTER))
+						.orElse(null), // optional
+				resolveItemValue(entry, source) // optional
+		);
 	}
 }
